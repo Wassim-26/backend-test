33b207c0b2fa78e755566e7530a886ac20831fa8
Wassim-26 <abderahmanewassim652@gmail.com>
2025-04-05 18:02:41 +0100

    add bcrypt-password

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import { drinksRouter } from "./routers/drink.js";
 import { sandwitchesRouter } from "./routers/sandwitche.js";
+import { userRouter } from "./routers/user.js";
 const app = express();
 app.use(express.json());
 
 app.use("/drinks", drinksRouter);
 app.use("/sandwitches", sandwitchesRouter);
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("welcome to FastFoodApi");
@@ -14,10 +16,6 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
   console.log("Server is running on PORT 5000");
 });
-mongoose
-  .connect(
-    "mongodb+srv://messiwassim2006:pass1%40@cluster0.hxod3k0.mongodb.net/wassim?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("Connected to DB");
-  });
+mongoose.connect("url").then(() => {
+  console.log("Connected to DB");
+});
