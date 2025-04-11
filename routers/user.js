2e3819f5ef1fe62cb411998f8d0d2630a53420f0
Wassim-26 <abderahmanewassim652@gmail.com>
2025-04-11 17:17:37 +0100

    fish the project

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { createUserRegister } from "../controllers/user.js";
+import { createUserRegister, getProfile, login } from "../controllers/user.js";
 
 export const userRouter = express.Router();
 userRouter.post("/register", createUserRegister);
+userRouter.post("/login", login);
+userRouter.post("/profile", auth, getProfile);
+userRouter.put("/update", update);
